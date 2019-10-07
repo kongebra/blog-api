afedc213b61db5db8123556b6f014528e6bcbfd7
Svein Are Danielsen <svein.danielsen@inmeta.no>
2019-10-07 19:24:56 +0200

    made simple http server with ping

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("Hello Vader!")
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("pong"))
+	})
+
+	log.Fatal(http.ListenAndServe(":5000", router))
 }
